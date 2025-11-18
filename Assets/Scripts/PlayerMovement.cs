1f33051f4e88838a94f4147c44858c5d81854785
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2025-11-18 00:33:57 -0500

    create: Folders & Basic Movement
    - Created some folders to organize our project
    
    - Create a basic movement system for the players
    
    - Used cinemachine to create a 3D camera perspective for the player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Paramters")]
+    [SerializeField] private float _movementSpeed;
+
+    [Header("Run Paramters")]
+    [SerializeField] private float _runMultiplier;
+
+    private Rigidbody _rb;
+    private Vector3 _vectorMovement;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+        Run();
+    }
+
+    void Movement()
+    {
+        _vectorMovement.x = Input.GetAxisRaw("Horizontal");
+        _vectorMovement.z = Input.GetAxisRaw("Vertical");
+
+        _rb.MovePosition(_rb.position + _vectorMovement * _movementSpeed * Time.fixedDeltaTime);
+    }
+
+    private void Run()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _movementSpeed *= _runMultiplier;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            _movementSpeed /= _runMultiplier;
+        }
+    }
+}
